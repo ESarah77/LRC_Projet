6b51071757bc45a1477bf9ba7f14c413180eac53
Doria0 <48170744+Doria0@users.noreply.github.com>
2023-12-10 17:00:16 +0100

    Update dmt.pl

--- dmt.pl
+++ dmt.pl
@@ -1,1 +1,48 @@
-%rien
+%--------------Partie I----------------
+% Mettre sous forme normale negative
+% récursion gauche: La cible la plus à gauche dans le corps d’une règle est la même
+% que la tête de la règle. aurait du pb mais pas grave.
+nnf(not(and(C1,C2)),or(NC1,NC2)):- nnf(not(C1),NC1), nnf(not(C2),NC2),!.
+nnf(not(or(C1,C2)),and(NC1,NC2)):- nnf(not(C1),NC1), nnf(not(C2),NC2),!.
+nnf(not(all(R,C)),some(R,NC)):- nnf(not(C),NC),!.
+nnf(not(some(R,C)),all(R,NC)):- nnf(not(C),NC),!.
+nnf(not(not(X)),Y):- nnf(X,Y),!.
+nnf(not(X),not(X)):-!.
+nnf(and(C1,C2),and(NC1,NC2)):- nnf(C1,NC1),nnf(C2,NC2),!.
+nnf(or(C1,C2),or(NC1,NC2)):- nnf(C1,NC1), nnf(C2,NC2),!.
+nnf(some(R,C),some(R,NC)):- nnf(C,NC),!.
+nnf(all(R,C),all(R,NC)) :- nnf(C,NC),!.
+nnf(X,X).
+
+% correction syntaxique & semantique
+
+% concept(X,Lres) :- concept().
+concept(X) :- setof(X,cnamea(X),L),!.
+concept(X) :- setof(X,cnamena(X),L),!.
+concept(X) :- setof(X,iname(X),L),!.
+concept(X) :- setof(X,rname(X),L),!.
+
+% ; pour "ou"
+
+% autoref().
+
+% Mettre sous forme normale negative
+% traitement-Tbox(Cx) :- concept-atomique(Cx); traitement-Tbox(Cx-qqch).
+
+% Deploiement de TBox
+% traitement-ABox(Ix).
+
+% Astuces:
+% =/2
+% 'mia'= mia -> true
+% '2' = 2 -> false
+% structure recursive + unification
+
+%--------------Partie II----------------
+
+%--------------Partie III----------------
+%Trouver les successeurs
+%individu(i).
+%individu(s(I)) :- individu(I)
+% res => i, s(i), s(s(i))...
+
